a30dda2a343226667d10f798bf4f60c1d8bbbb5b
sjy <jysh0923@naver.com>
2020-09-14 19:31:02 +0900

    error modify2

--- myapi/settings.py
+++ myapi/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
